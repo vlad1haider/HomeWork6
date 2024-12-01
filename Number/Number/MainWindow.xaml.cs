d87306c084a14b487dce525fada1abaa1a4e0fbd
Haider <59825081+vlad1haider@users.noreply.github.com>
2024-12-01 12:33:55 +0500

    Add files via upload

---
+++ Number/Number/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfYieldReturnApp
+{
+    public partial class MainWindow : Window
+    {
+        private NumberGenerator _numberGenerator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _numberGenerator = new NumberGenerator();
+        }
+
+        private async void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            NumbersListBox.Items.Clear(); 
+
+            if (int.TryParse(StartTextBox.Text, out int start) && int.TryParse(EndTextBox.Text, out int end))
+            {
+                await GenerateNumbersAsync(start, end);
+                MessageBox.Show("Number generation complete!");
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integer values for start and end.");
+            }
+        }
+
+        private async Task GenerateNumbersAsync(int start, int end)
+        {
+            await Task.Run(() =>
+            {
+                foreach (var number in _numberGenerator.GenerateNumbers(start, end))
+                {
+                    // Update the UI on the main thread
+                    Dispatcher.Invoke(() => NumbersListBox.Items.Add(number));
+                }
+            });
+        }
+    }
+}
---
+++ Number/Number/NumberGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+public class NumberGenerator
+{
+    public IEnumerable<int> GenerateNumbers(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Thread.Sleep(500); 
+            yield return i; 
+        }
+    }
+}
